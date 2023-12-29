3526bc5a47a5f0813b17fc1eef4b350d182a22c2
Jatin Patil <jatinpatil2003@gmail.com>
2023-12-29 22:57:25 +0530

    Added fusion exported model

---
+++ autonav_description/launch/robot_description.launch.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("autonav_description"),
+                    "urdf",
+                    "autonav.xacro",
+                ]
+            ),
+
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[robot_description]),
+    ])
