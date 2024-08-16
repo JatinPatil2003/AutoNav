686dacf341483a2354be37b40a8a1be0ccc84a0e
Jatin Patil <jatinpatil2003@gmail.com>
2024-08-16 23:35:44 +0530

    fixed flake8 percpt

--- autonav_perception/autonav_perception/camera_subscribe.py
+++ autonav_perception/autonav_perception/camera_subscribe.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+import cv2
+from cv_bridge import CvBridge
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image, CompressedImage
-from cv_bridge import CvBridge
-import cv2
+from sensor_msgs.msg import CompressedImage
 
 class ImageSubscriber(Node):
+
     def __init__(self):
         super().__init__('image_subscriber')
         self.subscription = self.create_subscription(
@@ -19,10 +20,11 @@ class ImageSubscriber(Node):
     def image_callback(self, msg):
         try:
             cv_image = self.bridge.compressed_imgmsg_to_cv2(msg)
-            cv2.imshow("Image", cv_image)
+            cv2.imshow('Image', cv_image)
             cv2.waitKey(1)
         except Exception as e:
             print(e)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -30,6 +32,7 @@ def main(args=None):
     rclpy.spin(image_subscriber)
     image_subscriber.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
